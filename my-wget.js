bd676cbbe28828ff25de92aa04e123e6a024bfde
Westindiess <gregoryalexiss@gmail.com>
2021-04-09 15:47:48 +0200

    exo-async-0

--- my-wget.js
+++ my-wget.js
@@ -1,6 +1,17 @@
 const axios = require("axios")
 const fsPromises = require("fs/promises")
 
+const ERR_INVALID_FILE_URL_PATH = { code: "ERR_INVALID_FILE_URL_PATH" }
+
+try {
+	if (process.argv.length !== 3) {
+		console.log(ERR_INVALID_FILE_URL_PATH)
+		process.exit(1)
+	}
+} catch (e) {
+	console.log(e.message)
+}
+
 const main = async () => {
 	try {
 		const response = await axios.get(process.argv[2])
